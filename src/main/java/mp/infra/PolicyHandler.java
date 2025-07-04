db598fe0c50e2f2533b0b05230f1d131a826e86c
upz <smilish67@naver.com>
2025-07-04 15:27:32 +0900

    temp

--- src/main/java/mp/infra/PolicyHandler.java
+++ src/main/java/mp/infra/PolicyHandler.java
@@ -54,36 +54,51 @@ public class PolicyHandler {
     // 결제 완료 이벤트 수신 (포인트 적립)
     @StreamListener(KafkaProcessor.PAYMENT_INPUT)
     public void whenPurchased_then_CREATE(@Payload Purchased purchased) {
-        log.info("결제 이벤트 수신: {}", purchased);
+        log.info("{\"event\":\"PAYMENT_RECEIVED\", \"type\":\"PAYMENT\", \"userId\":\"{}\", \"amount\":{}, \"item\":\"{}\"}", 
+            purchased.getUserId(), purchased.getAmount(), purchased.getItem());
+            
         try {
             // 구독 결제인 경우 포인트 적립하지 않음
             if (purchased.getItem() != null && purchased.getItem().toLowerCase().contains("subscription")) {
-                log.info("구독 결제로 인한 포인트 적립 제외 - 사용자: {}", purchased.getUserId());
+                log.info("{\"event\":\"PAYMENT_SKIPPED\", \"type\":\"SUBSCRIPTION\", \"userId\":\"{}\"}", 
+                    purchased.getUserId());
                 return;
             }
             
             Point point = pointsService.addPointForPayment(purchased.getUserId(), purchased.getAmount());
-            updatePointView(point);
+            log.info("{\"event\":\"POINTS_ADDED\", \"type\":\"PAYMENT\", \"userId\":\"{}\", \"amount\":{}, \"totalPoints\":{}}", 
+                purchased.getUserId(), purchased.getAmount(), point.getTotalPoint());
+                
         } catch (Exception e) {
-            log.error("결제 포인트 적립 실패", e);
+            log.error("{\"event\":\"POINTS_ADD_FAILED\", \"type\":\"PAYMENT\", \"userId\":\"{}\", \"amount\":{}, \"error\":\"{}\"}", 
+                purchased.getUserId(), purchased.getAmount(), e.getMessage());
         }
     }
 
     // 책 구매 이벤트 수신 (포인트 사용)
     @StreamListener(KafkaProcessor.BOOK_INPUT)
     public void whenBookPurchased_then_CREATE(@Payload BookPurchased bookPurchased) {
-        log.info("책 구매 이벤트 수신: {}", bookPurchased);
+        log.info("{\"event\":\"BOOK_PURCHASE_RECEIVED\", \"userId\":\"{}\", \"bookId\":\"{}\", \"point\":{}, \"title\":\"{}\"}", 
+            bookPurchased.getUserId(), bookPurchased.getBookId(), 
+            bookPurchased.getPoint(), bookPurchased.getTitle());
+            
         try {
             // 포인트 사용이 0인 경우 (구독자) 처리하지 않음
             if (bookPurchased.getPoint() == null || bookPurchased.getPoint() == 0) {
-                log.info("구독자 구매 - 포인트 차감 없음: userId={}", bookPurchased.getUserId());
+                log.info("{\"event\":\"POINTS_USAGE_SKIPPED\", \"type\":\"SUBSCRIPTION\", \"userId\":\"{}\", \"bookId\":\"{}\"}", 
+                    bookPurchased.getUserId(), bookPurchased.getBookId());
                 return;
             }
             
             Point point = pointsService.usePointForBook(bookPurchased.getUserId(), bookPurchased.getPoint());
-            updatePointView(point);
+            log.info("{\"event\":\"POINTS_USED\", \"userId\":\"{}\", \"bookId\":\"{}\", \"usedPoints\":{}, \"remainingPoints\":{}, \"title\":\"{}\"}", 
+                bookPurchased.getUserId(), bookPurchased.getBookId(), 
+                bookPurchased.getPoint(), point.getTotalPoint(), bookPurchased.getTitle());
+                
         } catch (Exception e) {
-            log.error("책 구매 포인트 차감 실패", e);
+            log.error("{\"event\":\"POINTS_USAGE_FAILED\", \"userId\":\"{}\", \"bookId\":\"{}\", \"point\":{}, \"error\":\"{}\"}", 
+                bookPurchased.getUserId(), bookPurchased.getBookId(), 
+                bookPurchased.getPoint(), e.getMessage());
         }
     }
 }
